36211dd00cc1ae006dfb68986102dbd029b73b24
Komal Priya <kryptonbenzene@gmail.com>
2026-01-19 08:19:54 +0530

    Reverse array question done

---
+++ Arrays/DAY-1/QuesNnotes/ReverseArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Q1{
+public static void main(String[] args) {
+    
+
+int[] arr={1,2,3,4};
+System.out.println(Arrays.toString(arr));
+int start=0;
+int end=arr.length-1;
+int temp;
+while (start < end) { 
+      temp=arr[start];
+    arr[start]=arr[end];
+    arr[end]=temp;
+    start++;
+    end--;
+} 
+  System.out.println(Arrays.toString(arr));
+
+    
+}
+
+
+
+
+
+};
