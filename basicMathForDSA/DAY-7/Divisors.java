34d3a007c731939a05b765f431ef027bd306d4b1
Komal Priya <kryptonbenzene@gmail.com>
2026-03-25 21:10:56 +0530

    feat(math): add print all divisor of numbers solution (day-7)

---
+++ basicMathForDSA/DAY-7/Divisors.java
@@ -0,0 +1,18 @@
+public class Divisors {
+    public static void printDivisors(int n) {
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+
+                if (i != n / i) {
+                    System.out.print(n / i + " ");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printDivisors(12);
+    }
+}
