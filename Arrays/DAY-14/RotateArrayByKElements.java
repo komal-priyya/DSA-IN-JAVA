35d9762f8104e21e3777469556e73654a6ed7e65
Komal Priya <kryptonbenzene@gmail.com>
2026-04-07 23:09:37 +0530

    feat(Arrays)- add Rotate array by k elements solution (day-14)

---
+++ Arrays/DAY-14/RotateArrayByKElements.java
@@ -0,0 +1,48 @@
+
+import java.util.Arrays;
+
+
+
+public class RotateArrayByKElements {
+    public static void LeftRotate(int[] arr1, int k) {
+  int n = arr1.length;
+        k = k % n;
+
+        Reverse(arr1, 0, k - 1);     
+        Reverse(arr1, k, n - 1);     
+        Reverse(arr1, 0, n - 1);
+    }
+    public static void RightRotate(int[] arr2, int k){
+   int n = arr2.length;
+        k = k % n;
+
+        Reverse(arr2, 0, n - 1);     
+        Reverse(arr2, 0, k - 1);     
+    }
+    public static void Reverse(int[] arr, int start, int end){
+ while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+    }}
+    public static void main(String[] args) {
+        
+
+        int[]arr1 ={1,2,3,4,5,6};
+        int k=2;
+        // LEFT ROTATE
+        LeftRotate(arr1,k);
+                System.out.println("Left Rotate by 2: " + Arrays.toString(arr1));
+
+
+         int[]arr2 ={1,2,3,4,5,6};
+    //   RIGHT ROTATE
+        RightRotate(arr2,k);
+                System.out.println("Right Rotate by 2: " + Arrays.toString(arr2));
+
+
+    }
+    }
+
