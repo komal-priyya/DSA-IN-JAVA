26be74f926a418f54e17f8c6b974de9cc817e563
Komal Priya <kryptonbenzene@gmail.com>
2026-04-25 22:33:05 +0530

    feat(Arrays)- updated code )

--- Arrays/DAY-23/LongestSubarray.java
+++ Arrays/DAY-23/LongestSubarray.java
@@ -5,7 +5,9 @@ Problem: longest subarray with given sum k
 
 
  Approach:
- Use a prefix sum and a hash map to store the first occurrence of each prefix sum. At index i, if prefixSum – k has been seen before, then there exists a subarray ending at i with sum exactly k
+ Use a prefix sum and a hash map to store the first occurrence of each prefix sum. At index i,
+  if prefixSum – k has been seen before, then there exists a subarray ending at i with sum 
+  exactly k
 
  
  
@@ -15,34 +17,41 @@ space complexity:O(1)
 */
 
 
-
-
-
-
-
-
-
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class LongestSubarray {
-    public static int longestSubarraywithsumk(int[] arr, int k) {
-        Map<Integer, Integer> prefixMap = new HashMap<>(); // prefixSum -> first index
+    public static List<Integer> longestSubarraywithsumk(int[] arr, int k) {
+        HashMap<Integer, Integer> prefixMap = new HashMap<>(); // prefixSum -> first index
         int prefixSum = 0;
         int maxLen = 0;
+        int start= -1;
+        int end= -1;
 
         for (int i = 0; i < arr.length; i++) {
             prefixSum += arr[i];
 
             // subarray from 0 to i has sum k
             if (prefixSum == k) {
-                maxLen = i + 1;
+                if(i+1> maxLen){
+                    maxLen = i + 1; 
+                    start= 0;
+                    end= i;
+                }
+               
             }
 
             // check if (prefixSum - k) exists
             if (prefixMap.containsKey(prefixSum - k)) {
-                int len = i - prefixMap.get(prefixSum - k);
-                maxLen = Math.max(maxLen, len);
+
+                int prevIndex= prefixMap.get(prefixSum - k);
+                int len = i- prevIndex;
+                if(len >maxLen){
+                    maxLen = len;
+                    start = prevIndex + 1;
+                    end= i;
+                }
             }
 
             // store first occurrence only
@@ -51,12 +60,22 @@ public class LongestSubarray {
             }
         }
 
-        return maxLen;
+        List<Integer> ans= new ArrayList<>();
+        if( start == -1){
+            return ans;
+        }
+        for(int i= start; i<=end; i++){
+            ans.add(arr[i]);
+        }
+        return ans;
+
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 5, 2, 7, 1, -10};
-        int k = 15;
-        System.out.println(longestSubarraywithsumk(arr, k)); // Output: 6
+        int[] arr = {1,2,3,1,1,1,1};
+        int k = 6;
+        List<Integer> result = longestSubarraywithsumk(arr, k);
+
+        System.out.println(result); 
     }
 }
