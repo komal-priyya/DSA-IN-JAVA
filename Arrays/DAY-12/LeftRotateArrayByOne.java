9b1b139b17cf73b5f79845f68125de6832f3fc4d
Komal Priya <kryptonbenzene@gmail.com>
2026-06-18 18:12:27 +0530

    updated code

--- Arrays/DAY-12/LeftRotateArrayByOne.java
+++ Arrays/DAY-12/LeftRotateArrayByOne.java
@@ -22,37 +22,41 @@ Space complexity: O(1)
 */ 
 import java.util.Scanner;
 
-
-
 public class LeftRotateArrayByOne {
- public static void LeftRotate(int[]arr, int k){
 
-int n= arr.length;
-int temp=arr[0];
-       for(int i=0;i<n;i++){
-             arr[i]=arr[i+1];
-           
-       }
-  arr[n-1]=temp;
+    public static void LeftRotate(int[] arr) {
 
+        int n = arr.length;
+        int temp = arr[0];
 
-
- }  
- public static void main(String[] args) {
-     
-        Scanner Inp= new Scanner(System.in);
-
-        System.out.println("Enter the size of  array");
-        int n = Inp.nextInt();
-int k=1; //steps
-        int[]arr= new int[n];
-        System.out.println("Enter the array elements");
-        for(int i=0 ; i<n;i++){
-       arr[i]=Inp.nextInt();
-
-
-       LeftRotate(arr,k);
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
         }
 
- } 
+        arr[n - 1] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Enter the size of array");
+        int n = inp.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = inp.nextInt();
+        }
+
+        LeftRotate(arr);
+
+        System.out.println("Array after left rotation:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
