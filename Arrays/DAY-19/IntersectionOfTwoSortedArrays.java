bf4a80d9cef74a89aa0cadf5aa3a560514befe95
Komal Priya <kryptonbenzene@gmail.com>
2026-07-11 16:56:41 +0530

    updated the approach

--- Arrays/DAY-19/IntersectionOfTwoSortedArrays.java
+++ Arrays/DAY-19/IntersectionOfTwoSortedArrays.java
@@ -5,25 +5,31 @@
 ->Use Two Pointer Techniquw since both arrays are sorted 
 
 -Initialize two pointers i and j at 0
--Traverse both arrays simultaneously
+
 
 --STEPS:
-1: compare arr1[i] and arr2[j]
-2: if arr1[i]<arr2[j] -> move i++(skip smaller element)
-3:if arr1[i]>arr2[j] -> move j++(skip smaller element)
-4: if arr1[i] == arr2[j]:
- --Add element to result (only once)
- -- Move both i++ and j++
-5:Avoid duplicates while adding
+1.Start with pointers i=0 and j=0
+2.Compare arr1[i] with arr2[j]:
 
-(Skip non-matching elements and only take common elements)
+3.If equal → Add to result, move both i++ and j++
+4.If arr1[i] < arr2[j] → Move i++ (arr1 needs to catch up)
+5.If arr1[i] > arr2[j] → Move j++ (arr2 needs to catch up)
 
---Time complexity: O(n + m)
--we traverse both arrays once using two pointers
+6.Continue until one pointer reaches the end
 
 
---Space complexity: O(k)  // for storing intersection elements     (k= number of common elements)
-                  O(1)  // auxiliary space (excluding result)
+Complexity
+
+-Time Complexity: O(n + m)
+
+--Each array traversed once
+--Total comparisons = n + m
+
+
+-Space Complexity: O(1)
+
+--Only two pointers (no extra data structures)
+--Result space doesn't count
 
 
 
@@ -65,14 +71,14 @@ return Intersection;
 int m = Inp.nextInt();
         int[] arr1 = new int[n];
 
-        int[] arr2 = new int[n];
+        int[] arr2 = new int[m];
 
         System.out.println("Enter the Elements of first Array ");
         for (int i = 0; i < n; i++) {
             arr1[i] = Inp.nextInt();
         }
 
-        System.out.println("Enter the Elements of first Array ");
+        System.out.println("Enter the Elements of second Array ");
         for (int i = 0; i < m; i++) {
             arr2[i] = Inp.nextInt();
         }
